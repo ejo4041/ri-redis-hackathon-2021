358b4ef471694b9c72138a0e2102ed354291d2fd
Brandon Beiler <bbeiler@ridgelineintl.com>
2021-05-06 16:57:19 -0400

    Adding additional rest endpoints to the admin controller

--- api/src/main/java/com/acme/caas/web/rest/AdminController.java
+++ api/src/main/java/com/acme/caas/web/rest/AdminController.java
@@ -1,6 +1,7 @@
 package com.acme.caas.web.rest;
 
 import com.acme.caas.domain.CaaSTemplate;
+import com.acme.caas.domain.responses.AdminControllerResponse;
 import com.acme.caas.domain.responses.AdminTemplatesResponse;
 import com.acme.caas.domain.responses.AdminTemplateResponse;
 import com.acme.caas.service.RedisService;
@@ -9,6 +10,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotNull;
+import java.util.MissingFormatArgumentException;
 
 @CrossOrigin
 @RestController
@@ -36,7 +40,7 @@ public class AdminController {
     @PostMapping(path = "/template/create", consumes = "application/json", produces = "application/json")
     public AdminTemplateResponse createTemplate(@RequestBody CaaSTemplate template) throws Exception {
         logger.info("/template/create");
-        logger.debug("Template: " + template.toString());
+        logger.debug("Template: " + template.getTemplateName());
 
         if(template.getSettingsId() != null){
             logger.error("The provided template already has a settingsId, which is ignored when creating " +
@@ -61,6 +65,52 @@ public class AdminController {
         AdminTemplatesResponse response = new AdminTemplatesResponse();
         response.setMessage("Templates retrieved!");
         response.setTemplateList(templates);
+        return response;
+    }
+
+    @GetMapping(path = "/template/get/{id}", consumes = "application/json", produces = "application/json")
+    public AdminTemplateResponse getTemplate(@PathVariable("id") String settingsId) throws Exception{
+        logger.info("/template/get/"+settingsId);
+
+        var template = redisService.getTemplate(settingsId);
+
+        AdminTemplateResponse response = new AdminTemplateResponse();
+        response.setMessage("Template retrieved!");
+        response.setTemplate(template);
+        return response;
+    }
+
+    @PutMapping(path = "/template/update", consumes = "application/json", produces = "application/json")
+    public AdminTemplateResponse updateTemplate(@RequestBody CaaSTemplate template) throws Exception {
+        logger.info("/template/update");
+        logger.debug("Template: " + template.toString());
+
+        if(template.getSettingsId() == null){
+            logger.error("The provided template does not have a settingsId, which is required in order to update " +
+                "the template");
+            throw new IllegalArgumentException("The CaasTemplate is missing a required settingsId value");
+        }
+
+        redisService.updateTemplate(template);
+
+        // Respond
+        AdminTemplateResponse response = new AdminTemplateResponse();
+        response.setMessage("Template Updated!");
+        response.setTemplate(template);
+        return response;
+    }
+
+    @PostMapping(path = "/template/update/{id}/templateName/{name}", produces = "application/json")
+    public AdminControllerResponse updateTemplate(@PathVariable("id") @NotNull String settingsId,
+                                                @PathVariable("name") @NotNull String templateName) throws Exception {
+        logger.info("/template/update/templateName");
+        logger.debug("Template: " + settingsId);
+
+        redisService.updateTemplateName(settingsId, templateName);
+
+        // Respond
+        AdminControllerResponse response = new AdminControllerResponse();
+        response.setMessage("Template name updated to " + templateName);
         return response;
     }
 }
