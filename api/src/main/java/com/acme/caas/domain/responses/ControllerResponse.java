bf2f2ae037193ea64e5807c7b003530aa9eeb1b1
Brandon Beiler <bbeiler@ridgelineintl.com>
2021-05-12 11:38:20 -0400

    New /client endpoint that allows clients to get and update (not create or delete) CaaSTemplates and Template Settings. See Swagger endpoint for full update (http://localhost:8081/swagger-ui.html#/client-controller).

---
+++ api/src/main/java/com/acme/caas/domain/responses/ControllerResponse.java
@@ -0,0 +1,15 @@
+package com.acme.caas.domain.responses;
+
+import java.util.List;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@SuperBuilder
+public class ControllerResponse {
+
+    private String message;
+}
